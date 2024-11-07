5714f6074510c19515f54ca7d77df433f3917109
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-11-07 20:09:07 +0100

    move sp1's start funciton to the sp1 dir
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,5 @@
 const std = @import("std");
 
-const STACK_TOP_ADDR = 0x0200_0400;
-
-export fn _start() linksection(".text._start") callconv(.Naked) void {
-    // set stack + global pointer up
-    asm volatile (
-        \\lw sp, 0(%[stack_top_addr])
-        \\call main
-        :
-        : [stack_top_addr] "{sp}" (STACK_TOP_ADDR),
-        : "memory"
-    );
-}
-
 export fn main() noreturn {
     const evm_bytecode = [_]u8{0};
     // simplistic evm
