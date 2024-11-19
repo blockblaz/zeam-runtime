90f7d0aa7591ce1481a8765d4c7c6b352e685bcb
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-11-19 06:40:31 +0100

    temporary hack to get it to build on the command line

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,32 @@
 const std = @import("std");
 
+extern const __global_pointer: u32;
+extern const __powdr_stack_start: u32;
+
+const syscall_halt: u32 = 9;
+
+export fn _start() callconv(.Naked) void {
+    // export fn _start() linksection(".text._start") callconv(.Naked) void {
+    // set stack + global pointer up
+    asm volatile (
+        \\.option push
+        \\.option norelax
+        \\tail main
+        :
+        : [global_pointer] "{gp}" (__global_pointer),
+          [powdr_stack_start] "{sp}" (__powdr_stack_start),
+        : "memory"
+    );
+}
+
+pub fn halt() noreturn {
+    asm volatile ("ecall"
+        :
+        : [scallnum] "{t0}" (syscall_halt),
+    );
+    while (true) {}
+}
+
 export fn main() noreturn {
     const evm_bytecode = [_]u8{0};
     // simplistic evm
