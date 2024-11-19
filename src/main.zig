cf17e4f416c7f5b4451e6b73dbf28b3f6354af4e
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-11-19 13:21:21 +0100

    quick cleanup
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,6 @@ extern const __powdr_stack_start: u32;
 const syscall_halt: u32 = 9;
 
 export fn _start() callconv(.Naked) void {
-    // export fn _start() linksection(".text._start") callconv(.Naked) void {
-    // set stack + global pointer up
     asm volatile (
         \\.option push
         \\.option norelax
