8116187ac60449bda68fbaca4b9dbf1f03172c90
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-11-27 12:38:52 +0100

    powdr: fix/refactor assembly of input functions
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- src/powdr/io.zig
+++ src/powdr/io.zig
@@ -13,34 +13,30 @@ pub fn print_str(str: []const u8) void {
 }
 
 pub fn read_u32(idx: u32) u32 {
-    var ret: u32 = undefined;
-    asm volatile ("ecall"
-        : [ret] "={a0}" (ret),
+    return asm volatile ("ecall"
+        : [ret] "={a0}" (-> u32),
         : [scallnum] "{t0}" (@intFromEnum(syscalls.input)),
           [fd] "{a0}" (0), // fd = 0 == stdin
           [idx] "{a1}" (idx + 1),
         : "memory"
     );
-    return ret;
 }
 
 pub fn read_data_len(fd: u32) usize {
-    var ret: u32 = undefined;
-    asm volatile ("ecall"
-        : [ret] "{a0}" (ret),
+    return asm volatile ("ecall"
+        : [ret] "={a0}" (-> usize),
         : [scallnum] "{t0}" (@intFromEnum(syscalls.input)),
           [subcommand] "{a0}" (fd),
           [idx] "{a1}" (0),
         : "memory"
     );
-    return ret;
 }
 
 pub fn read_slice(fd: u32, data: []u32) void {
     for (data, 0..) |*d, idx| {
         var item: u32 = undefined;
         asm volatile ("ecall"
-            : [ret] "{a0}" (item),
+            : [ret] "={a0}" (item),
             : [scallnum] "{t0}" (@intFromEnum(syscalls.input)),
               [subcommand] "{a0}" (fd),
               [idx] "{a1}" (idx + 1),
