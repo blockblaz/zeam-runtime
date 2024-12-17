2eb4f24dc32228a351849c523bebc524872540bf
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-12-17 11:33:06 +0100

    build: rename executable to zeam-runtime
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "zeam-poc",
+        .name = "zeam-runtime",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
